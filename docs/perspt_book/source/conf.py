93dbfe0aeff5408446024d4f539515276f1f23a8
Ronak Rathore <ronak@vyomtech.com>
2025-06-11 12:36:21 +0800

    chore: upgrade perspt book version to match the release verion.

--- docs/perspt_book/source/conf.py
+++ docs/perspt_book/source/conf.py
@@ -12,8 +12,8 @@ import sys
 project = 'Perspt'
 copyright = '2025, Ronak Rathore, Vikrant Rathore'
 author = 'Ronak Rathore, Vikrant Rathore'
-release = '0.4.0'
-version = '0.4.0'
+release = '0.4.4'
+version = '0.4.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
